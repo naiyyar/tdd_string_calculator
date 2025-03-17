769a24226f82a1dd7eb7882b50e6d75294cb2680
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-03-17 18:19:27 +0530

    refactoring: extracting custom delimiter code move to separate method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,11 @@
 class StringCalculator
-  def self.add string
+  def self.add(string)
     return 0 if string.empty?
 
     delimiter = ','
 
     if string.start_with?('//')
-      delimiter_str, *string = string.split("\n") # *string just for this case "//;\n1;2\n3"
-      delimiter = delimiter_str[2..]
-
-      string = string.join(delimiter) # Handling extra case "//;\n1;2\n3" or "//;\n1;2\n3:1"
+      delimiter, string_numbers = extract_custom_delimiter(string)
     end
 
     string = string.gsub("\n", delimiter) if string.include?("\n")
@@ -19,5 +16,14 @@ class StringCalculator
     raise "negative numbers not allowed #{negative_numbers.join(', ')}" unless negative_numbers.empty?
 
     number_array.sum
+  end
+
+  def self.extract_custom_delimiter(string)
+    delimiter_str, *string = string.split("\n") # *string just for this case "//;\n1;2\n3"
+    custom_delimiter = delimiter_str[2..]
+
+    string = string.join(custom_delimiter) # Handling extra case "//;\n1;2\n3" or "//;\n1;2\n3:1"
+
+    return custom_delimiter, string
   end
 end
